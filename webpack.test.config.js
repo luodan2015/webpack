7edd07c376329300bb270a5aebd9597b3481c12c
luodan <1058470680@qq.com>
2021-02-20 11:36:33 +0800

    feat: cross-env的使用

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -4,9 +4,12 @@ const baseConfig = require('./webpack.config.base.js');
 const devConfig = require('./webpack.config.dev.js');
 const proConfig = require('./webpack.config.pro.js');
 
+console.log(process.env.NODE_ENV);
+
 module.exports = (env) => {
   // 如果外部传进env.production是生产环境，否则是开发环境
   if (env && env.production) {
+    console.log('env.production - ', env.production);
     return merge(baseConfig, proConfig);
   }
   return merge(baseConfig, devConfig);
